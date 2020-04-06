24470196c3a48eadf7763eb0b33ef92021df22c2
Sibanye <sibanyebukani01@gmail.com>
2020-04-06 05:26:57 +0200

    first kasi website

---
+++ pages/home.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <title>Kasi Mall</title>
+</head>
+
+<body>
+
+    <section>
+        <div class="container-fluid center justify-content-center" style="margin: 0 auto;">
+            <div class="brand">
+                <center><a href="#">KASI MALL ONLINE</a></center>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="navbar navbar-expand-lg nav justify-content-center sticky-top">
+            <div class="container-fluid m-0 p-0">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcollapseCMS">
+                    <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+                    <ul class="navbar-nav mx-auto">
+                        <li>
+                            <a href="#" class="nav-link">Register</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link">Merchant</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link">Driver</a>
+                        </li>
+                </div>
+</div>
+    </section>
+
+    <main>
+        <section class="showcase text-center ">
+            <div class="container-fluid">
+                <div class="row">
+                    <!--Left side of Showcase-->
+
+                    <!-- end of Left side of Showcase-->
+
+
+                    <!--Right side of Showcase-->
+                    <div class="col-sm-12 col-lg-12">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="card">
+                                        <div class="p-0 m-0 card-heading">
+                                            <h4>Amapapa</h4>
+                                        </div>
+
+                                        <div class="hvrbox">
+                                            <img src='../images/food.jpg' alt="kasi" class="hvrbox-layer_bottom ">
+                                            <div class="hvrbox-layer_top hvrbox-layer_scale">
+                                                <div class="hvrbox-text">AMAPAPA</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="card">
+                                        <div class=" p-0 m-0 card-heading">
+                                            <h4>Fashion</h4>
+                                        </div>
+                                        <div class="hvrbox">
+                                            <img src='../images/fashion.jpg' alt="kasi" class="hvrbox-layer_bottom img-fluid">
+                                            <div class="hvrbox-layer_top hvrbox-layer_scale">
+                                                <div class="hvrbox-text">AMAPAPA</div>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="card">
+                                        <div class=" p-0 m-0 card-heading">
+                                            <h4>Electronics</h4>
+                                        </div>
+                                        <div class="hvrbox">
+                                            <img src='../images/food.jpg' alt="kasi" class="hvrbox-layer_bottom img-fluid">
+                                            <div class="hvrbox-layer_top hvrbox-layer_scale">
+                                                <div class="hvrbox-text">AMAPAPA</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="card">
+                                        <div class="p-0 m-0 card-heading">
+                                            <h4>Spinza</h4>
+                                        </div>
+                                        <img src='../images/spinza2.jpg' class='card-img-top img-fluid showcase-img' alt='food Image'>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="card">
+                                        <div class=" p-0 m-0 card-heading">
+                                            <h4>Shops<h4>
+                                        </div>
+                                        <img src='../images/shops.jpg' class='card-img-top img-fluid showcase-img' alt='food Image'>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="card">
+                                        <div class=" p-0 m-0 card-heading">
+                                            <h4>Kasi Brands</h4>
+                                        </div>
+                                        <img src='../images/tshisa-nyama.jpg' class='card-img-top img-fluid showcase-img' alt='food Image'>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+
+
+</html>
